73a7e8be468439fdfa28b3e966c792981cf3ef2e
lhy199661 <lhy19960601@gmail.com>
2026-05-13 02:08:51 +0800

    For covid period
    Exclude the covid period

--- U.S. Election.R
+++ U.S. Election.R
@@ -494,16 +494,20 @@ simulate_trending_ou <- function(n,
 
 # ----Uploading the gambling data for 2020 and 2024 U.S. Election. Since the data have already done the cleaning, we do not need to do the data cleaning----.
 # ----2020 data as training data----
+# This is full dataset
 c<-read.csv("2020USElection.csv",header = T)
+# After we exclude the covid time
+d<-read.csv("2020USElectionwithoutcovid.csv",header=T)
+
 # 2024 data as test data
 b<-read.csv("2024USElection.csv",header = T)
 # Y is sum of implied probabilities of two candidates.
 # For 2020 data,
-#Y<-c$Total
+#Y<-d$Total
 # For 2024 data 
 Y<-b$Prob
 
-set.seed(123)
+set.seed(239539)
 n <- length(Y)
 # Set the training size: 10000, based on the length of the whole data.
 train_size <- 10000
@@ -578,10 +582,46 @@ Sys.setlocale("LC_TIME", "English")
 time <- as.POSIXct(b$timestampLON,
                    format="%Y/%m/%d %H:%M")
 #If we deal with 2020 dataset, we should exclude the Covid period.
-# time<-append(time,rep(NA,1079),after=11593)
+# time <- as.POSIXct(c$timestampLON,format="%Y/%m/%d %H:%M")
 # Y<-append(Y,rep(NA,1079),after=11593)
+# r<-append(r,rep(NA,1079),after=11593)
 # monitor_result$upper<-append(monitor_result$upper,rep(NA,1079),after=11593)
+# Ul<-l$Upper_cal
+# Ul<-append(Ul,rep(NA,1079),after=11593)
+# Ul_break <- Ul
+# filtered <- ifelse(filtered < 11594, filtered, filtered + 1079)
 
+# Define the 2020 dataset
+# df_Yp <- data.frame(time = time, Y = Y)
+# df_covid <- data.frame(time = time, covid = r)
+# df_Ul <- data.frame(time = time, Ul = Ul_break)
+
+# df_points <- data.frame(time = time[filtered],Y = Y[filtered])
+
+
+# q <- ggplot() +
+#  geom_line(data = df_Yp, aes(x = time, y = Y), color = "gray60") +
+#  geom_line(data = df_covid, aes(x = time, y = r), color = "green3") +
+  
+#  geom_line(data = df_Ul, aes(x = time, y = Ul), color = "red") +
+  
+#  geom_point(
+#    data = df_points,
+#    aes(x = time, y = Y),
+#    shape = 4, color = "blue", size = 1
+#  ) +
+#  geom_vline(
+#    xintercept = time[train_size],
+#   color = "magenta",
+#    linetype = "dashed"
+#  ) +
+#  geom_vline(xintercept = time[11593], color = "orange", linetype = "dashed") +
+#  geom_vline(xintercept = time[12673], color = "orange", linetype = "dashed") +
+  
+#  theme_bw() +
+#  labs(x = "Date", y = "Sum Of Probability")
+
+# Define the 2024 dataset
 df <- data.frame(
   time = time,
   Y = Y,
